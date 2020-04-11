f52df95ebca12ffb40adb2c5f09a31cd9485da90
Sachin Indoriya <57168948+codersachin26@users.noreply.github.com>
2020-04-11 20:15:13 +0530

    Update settings.py

--- ChillFlix/settings.py
+++ ChillFlix/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '7f=rrngc24nc+s+4oh$uy#mq)^pb&2bn@-98)4_0d=^wfswemr'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['mychillflix.herokuapp.com','127.0.0.1']
 
