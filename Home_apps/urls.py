0775919800ad043093af6afdc122b2ef1e2ca37c
sachin <sachinindoriya63@gmail.com>
2021-01-17 15:58:25 +0530

    implemented serach btn

--- Home_apps/urls.py
+++ Home_apps/urls.py
@@ -9,6 +9,7 @@ urlpatterns =[
     path('download_movie/<int:id>/<str:quality_type>',views.download_movie,name='download_movie'),
     path('category_by/<str:category_name>',views.category_by,name='category_by'),
     path('latest_movies',views.latest_movies,name='latest_movies'),
+    path('find_movie',views.find_movie,name='find_movies'),
   
 
 ]
--- Home_apps/views.py
+++ Home_apps/views.py
@@ -5,13 +5,13 @@ from datetime import datetime
 
 
 
-
+## mychillflix home page
 def index(request):
     movies = Movie.objects.all()[:10]
     count = None
     return render(request,'home.html',{'movies':movies,'count':count})
 
-
+## return all information about the movie
 def movie_info(request,id):
     movie            = Movie.objects.get(id=id)
     movie_categories = Category.objects.filter(Movie_id=id)
@@ -27,11 +27,12 @@ def movie_info(request,id):
     context = {'movie':movie,'categories':category,'screenshots':screenshots,'qualities':qualities}
     return render(request,'movie_info.html',context)
 
+## play the movie 
 def play_movie(request,id,quality_type):
     movie = Movie_file.objects.get(Movie_id=id,Quality_type=quality_type)
     return render(request,'play_movie.html',{'movie':movie})
 
-
+## send movie file to the user/browser
 def download_movie(request,id,quality_type):
     movie = Movie_file.objects.get(Movie_id=id,Quality_type=quality_type)
     file_name = movie.File.name
@@ -41,28 +42,35 @@ def download_movie(request,id,quality_type):
     http_response['Content-Disposition'] = f'attachment; filename={file_name}'
     return http_response
 
-
+## display movies by category name
 def category_by(request,category_name):
     movies = Category.objects.filter(Name=category_name)
-    cat_by_movies = []
-    for movie in movies:
-        cat_by_movies.append(movie.Movie)
+    if movies.exists():
+        cat_by_movies = []
+        for movie in movies:
+            cat_by_movies.append(movie.Movie)
+        return render(request,'home.html',{'movies':cat_by_movies})
 
-    return render(request,'home.html',{'movies':cat_by_movies})
+    return render(request,'not_found.html')
 
     
 
-
+## return latest released movies
 def latest_movies(request):
     current_year = f'{datetime.now().year}-1-1'
     movies = Movie.objects.filter(Released_date__gte=current_year)
     return render(request,'home.html',{'movies':movies})
 
+## return movie by serach string
+def find_movie(request):
+    movie_name = request.POST.get('movie_name')
+    movie_name = movie_name.title()
+    movie = Movie.objects.filter(Name=movie_name)
+    if movie.exists():
+        return render(request,'home.html',{'movies':movie})
+    return render(request,'not_found.html')
 
-# def find_movie(request):
-#     movie_name = request.GET.get('search')
-#     movie = Movie_info.objects.get(M_name=movie_name)
-#     return render(request,'searchmovie.html',{'i':movie})
+
 
 
 
